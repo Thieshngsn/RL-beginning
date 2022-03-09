5a475b5e6169ed31343c3d4fb2f799171dffd022
Thies Henningsen <thieshngs@gmail.com>
2022-03-09 14:07:53 +0100

    First step towards an CartPole Agent

---
+++ cartpole_experiment/main.py
@@ -0,0 +1,37 @@
+import random
+from abc import ABC
+from typing import TypeVar
+import random
+import gym
+
+Action = TypeVar('Action')
+
+
+class RandomActionWrapper(gym.ActionWrapper):
+    def __init__(self, env, epsilon=0.1):
+        super(RandomActionWrapper, self).__init__(env)
+        self.epsilon = epsilon
+
+    def action(self, action: Action) -> Action:
+        if random.random() < self.epsilon:
+            print("Random!")
+            return self.env.action_space.sample()
+        return action
+
+
+if __name__ == '__main__':
+    env = RandomActionWrapper(gym.make('CartPole-v1'))
+    reward = 0.0
+    steps = 0.0
+    obs = env.reset()
+
+    while True:
+        env.render()
+        action = env.action_space.sample()
+        obs, _reward, done, _ = env.step(action)
+        reward += _reward
+        steps += 1
+        if done:
+            break
+
+    print(f'reward: {reward}, steps: {steps}')
