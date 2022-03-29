57cbc27d7520d31ca5eb8a979bd31744867f208f
Thies Henningsen <thieshngs@gmail.com>
2022-03-29 06:54:22 +0200

    completed pong game
    trained an agent, saved the net in pong.joblib

--- pong/screen.py
+++ pong/screen.py
@@ -20,7 +20,7 @@ win = turtle.Screen()    # Create a screen
 win.title('Paddle')      # Set the title to paddle
 win.bgcolor('black')     # Set the color to black
 win.tracer(0)
-win.setup(width=500, height=800)   # Set the width and height to 600
+win.setup(width=600, height=600)   # Set the width and height to 600
 
 
 
@@ -31,7 +31,7 @@ paddle.speed(0)
 paddle.shapesize(stretch_wid=1, stretch_len=5)   # Streach the length of square by 5 
 paddle.penup()
 paddle.color('white')       # Set the color to white
-paddle.goto(0, -365)        # Place the shape on bottom of the screen
+paddle.goto(0, -265)        # Place the shape on bottom of the screen
 
 # Ball
 ball = turtle.Turtle()      # Create a turtle object
@@ -39,18 +39,18 @@ ball.speed(0)
 ball.shape('circle')        # Select a circle shape
 ball.color('red')           # Set the color to red
 ball.penup()
-ball.goto(0, -256)           # Place the shape in middle
+ball.goto(0, -156)           # Place the shape in middle
 
 # Paddle Movement
 def paddle_right():
     x = paddle.xcor()        # Get the x position of paddle
-    if x < 230:
-        paddle.setx(x+2)    # increment the x position by 20
+    if x < 280:
+        paddle.setx(x+5)    # increment the x position by 20
 
 def paddle_left():
     x = paddle.xcor()        # Get the x position of paddle
-    if x > -240:
-        paddle.setx(x-2)    # decrement the x position by 20
+    if x > -290:
+        paddle.setx(x-5)    # decrement the x position by 20
 
 # Keyboard Control
 left = WatchedKey('Left')
@@ -81,31 +81,31 @@ while True:   # same loop4
     
     
     # Ball-Walls collision  
-    if ball.xcor() > 240:    # If ball touch the right wall
-        ball.setx(240)
+    if ball.xcor() > 290:    # If ball touch the right wall
+        ball.setx(290)
         ball.dx *= -1        # Reverse the x-axis velocity
 
-    if ball.xcor() < -240:   # If ball touch the left wall
-        ball.setx(-240)
+    if ball.xcor() < -290:   # If ball touch the left wall
+        ball.setx(-290)
         ball.dx *= -1        # Reverse the x-axis velocity
 
-    if ball.ycor() > 390:    # If ball touch the upper wall
-        ball.sety(390)
+    if ball.ycor() > 290:    # If ball touch the upper wall
+        ball.sety(290)
         ball.dy *= -1        # Reverse the y-axis velocity
         #ball.dx = random.random() * 10-5
 
     # Ball-Paddle collision
-    if abs(ball.ycor() + 350) < 2 and abs(paddle.xcor() - ball.xcor()) < 55:
+    if abs(ball.ycor() + 250) < 2 and abs(paddle.xcor() - ball.xcor()) < 55:
         ball.dy *= -1
         if random.random() > 0.5:
-            ball.dx = random.random() * 2 + 2  # ball's x-axis velocity
+            ball.dx = random.random() * 8 + 2  # ball's x-axis velocity
         else:
-            ball.dx = -random.random() * 2 -2   # ball's x-axis velocity
+            ball.dx = -random.random() * 8 -2   # ball's x-axis velocity
 
     # Ball-Ground collison            
-    if ball.ycor() < -390:   # If ball touch the ground 
-        ball.goto(0, -265)    # Reset the ball position  
-        ball.dx = random.random() + 3   # ball's x-axis velocity 
+    if ball.ycor() < -290:   # If ball touch the ground 
+        ball.goto(0, -165)    # Reset the ball position  
+        ball.dx = random.random() * 4 + 3   # ball's x-axis velocity 
         ball.dy = 5  # ball's y-axis velocity 
 
      #ball.setx(ball.xcor() + dx)  # update the ball's x-location using velocity
